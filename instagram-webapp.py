ada4150818aee84d2938d49fa2d569f832a030b7
Andrew Kiggins <andrewkiggins2000@gmail.com>
2022-01-08 12:45:10 -0800

    MVP for streamlit

---
+++ instagram-webapp.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from modelHelper import judge_image
+
+st.write("""
+        # Instagram image judge
+        using ml to pick the image that will do the best on instagram
+""")
+
+image_files = st.file_uploader("Pick your images", type = ['png','jpeg','jpg'], accept_multiple_files=True)
+
+if image_files is not None:
+
+    image_score_pairs = {}
+
+    for image_file in image_files:
+        image_score = float(judge_image(image_file))
+        image_score_pairs[image_file] = image_score
+    
+    image_score_pairs = dict(sorted(image_score_pairs.items(), key = lambda item: item[1]))
+    
+    
+    st.write("""
+            ### The images below are order from what will do the best to what will do the worst in terms of likes
+            """)
+
+    for image in reversed(image_score_pairs):
+
+        st.image(image, caption = image_score_pairs[image]) 
---
+++ modelHelper.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import argparse
+import torch
+import torchvision.models
+import torchvision.transforms as transforms
+from PIL import Image
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def prepare_image(image):
+    if image.mode != 'RGB':
+        image = image.convert("RGB")
+    Transform = transforms.Compose([
+            transforms.Resize([224,224]),      
+            transforms.ToTensor(),
+            ])
+    image = Transform(image)   
+    image = image.unsqueeze(0)
+    return image.to(device)
+
+
+def predict(image, model):
+    image = prepare_image(image)
+    with torch.no_grad():
+        preds = model(image)
+    print(r'Popularity score: %.2f' % preds.item())
+    return preds.item()
+
+
+def judge_image(image_path):
+
+    image = Image.open(image_path)
+    model = torchvision.models.resnet50()
+    # model.avgpool = nn.AdaptiveAvgPool2d(1) # for any size of the input
+    model.fc = torch.nn.Linear(in_features=2048, out_features=1)
+    model.load_state_dict(torch.load('model/model-resnet50.pth', map_location=device)) 
+    model.eval().to(device)
+    return predict(image, model)
+
